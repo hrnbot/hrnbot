b547a2c140b9346a2f8d93852eebf6724b23df1c
hrnbot <hhiren111111@gmail.com>
2020-08-20 20:19:55 +0530

    Rearrang

--- index.js
+++ index.js
@@ -17,14 +17,11 @@ function generateProgressBar() {
 const readme = `\
 
 ### Hi there <img src="https://media.giphy.com/media/hvRJCLFzcasrR4ia7z/giphy.gif" width="25px">,  Hiren Namera here
+⏳ Year progress { ███████████████████▁▁▁▁▁▁▁▁▁▁▁ } 63.49 %
+---
+⏰ Updated on Thu, 20 Aug 2020 09:20:15 GMT
+
 <h2>My public Repos</h2>
-
-<p align="center">
-  Visitor count<br>
-  <img src="https://profile-counter.glitch.me/hrnbot/count.svg" />
-</p>
-
-
 <table>
   <thead align="center">
     <tr >
@@ -43,25 +40,16 @@ const readme = `\
   </tbody>
 </table>
 <p><a href="https://github.com/hrnbot" target="_blank"><img src="https://github.com/hrnbot/hrnbot/workflows/Progress%20Bar%20CI/badge.svg"/></a>
-<h2>Where to find me</h2>
-<p><a href="https://github.com/hrnbot" target="_blank"><img alt="Github" src="https://img.shields.io/badge/GitHub-%2312100E.svg?&style=for-the-badge&logo=Github&logoColor=white" /></a> <a href="https://www.linkedin.com/in/hiren-namera-83520ab4" target="_blank"><img alt="LinkedIn" src="https://img.shields.io/badge/linkedin-%230077B5.svg?&style=for-the-badge&logo=linkedin&logoColor=white" /></a> <a href="https://www.instagram.com/the_artificial_intel_developer/" target="_blank"><img alt="Instagram" src="https://img.shields.io/badge/instagram-%23E4405F.svg?&style=for-the-badge&logo=instagram&logoColor=white" /></a>
-</p>
+
 
 <h1>GitHub Stats</h1>
 <p><img src="https://github-readme-stats.vercel.app/api?username=hrnbot&amp;show_icons=true" alt="GitHub Stats"></p>
 <p><img src="https://github-readme-stats.vercel.app/api/top-langs/?username=hrnbot&layout=compact;show_icons=true"></p>
-=======
-### Hi there 👋
-⏳ Year progress { ███████████████████▁▁▁▁▁▁▁▁▁▁▁ } 63.49 %
----
-⏰ Updated on Thu, 20 Aug 2020 09:20:15 GMT
-![Progress Bar CI](https://github.com/hrnbot/hrnbot/workflows/Progress%20Bar%20CI/badge.svg)
->>>>>>> 382e886ef34bc73f2f695f586c37ca290b52126c
 
-⏳ Year progress ${progressBarOfThisYear} ${(progressOfThisYear * 100).toFixed(2)} %
----
-⏰ Updated on ${new Date().toUTCString()}
-![Progress Bar CI](https://github.com/hrnbot/hrnbot/workflows/Progress%20Bar%20CI/badge.svg)\
+<h2>Where to find me</h2>
+<p><a href="https://github.com/hrnbot" target="_blank"><img alt="Github" src="https://img.shields.io/badge/GitHub-%2312100E.svg?&style=for-the-badge&logo=Github&logoColor=white" /></a> <a href="https://www.linkedin.com/in/hiren-namera-83520ab4" target="_blank"><img alt="LinkedIn" src="https://img.shields.io/badge/linkedin-%230077B5.svg?&style=for-the-badge&logo=linkedin&logoColor=white" /></a> <a href="https://www.instagram.com/the_artificial_intel_developer/" target="_blank"><img alt="Instagram" src="https://img.shields.io/badge/instagram-%23E4405F.svg?&style=for-the-badge&logo=instagram&logoColor=white" /></a>
+</p>
+
 `
 
 console.log(readme)
