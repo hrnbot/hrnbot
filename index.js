6dff0c2f87c900fd3f8e9b1323ebeda0e5888d99
Hiren Namera <30949020+hrnbot@users.noreply.github.com>
2022-01-13 08:59:38 +0530

    Index updated for new tech added

--- index.js
+++ index.js
@@ -25,7 +25,8 @@ My Skills are improving day by day. Mathematics and  Physics are the skills in w
 
 I am working in the field of AI/ML and AR. I am a Detail-oriented engineer, with get-it-done, on-time, and best quality products. I had proven my skills in AI by modeling some state of art architectures and algorithms and proved skills of AR by writing multi-purpose AR modules that work seamlessly on Js, Python, C#, CPP, PHP (More to Come).
 
-As a senior developer in AI/ML/DL/computer vision and NLP learned how to train and test model architectures in Pytorch, Keras, Tensorflow, Theano, Deepstream, Tensorflow lite, TensorRT, Transformer etc... How to perform transfer learning for quick training. How to Combine multiple models and how to change architectures of underlying layers, build model architecture from scratch, etc... 
+As a senior developer in AI/ML/DL/computer vision and NLP learned how to train and test model architectures in Pytorch, 
+	, Tensorflow, Theano, Deepstream, Tensorflow lite, TensorRT, Transformer etc... How to perform transfer learning for quick training. How to Combine multiple models and how to change architectures of underlying layers, build model architecture from scratch, etc... 
 
 As a team leader of the AI and AR team learned to manage a team and take most out of team members. Train them to accept and complete future challenges and helping them to improve those skills in which they are lacking. Also learned how to use agile methodology to complete any project while keeping the team motivated and client satisfied. I was responsible for understanding client requirements and business prototype and help them to solve their problem in a modern way including enhancement of user experience, financial growth, increase in production capacity or productivity of employees, reach out to more customers, etc...
 
@@ -35,8 +36,19 @@ For more information, guidance or help feel free to Connect. Let's reduce humun 
 </p></details>
 <div>
 <h1><b>Skills</b></h1>
+<h2>:file_cabinet: Programming Language</h2>
+<img src="https://img.shields.io/badge/Python-3776AB?style=for-the-badge&logo=python&logoColor=white" /> 
+<img src="https://img.shields.io/badge/C-3776AB?style=for-the-badge&logo=c&logoColor=white" /> 
+<img src="https://img.shields.io/badge/CPP-3776AB?style=for-the-badge&logo=cpp&logoColor=white" /> 
+<img src="https://img.shields.io/badge/C-Sharp-3776AB?style=for-the-badge&logo=c-sharp&logoColor=white" /> 
+</div>
+
 <h2>🤖 Artificial Intelligence </h2>
-<img src="https://img.shields.io/badge/TensorFlow-3776AB?style=for-the-badge&logo=tensorflow&logoColor=white" /> 	
+<div>
+<img src="https://img.shields.io/badge/TensorFlow-3776AB?style=for-the-badge&logo=tensorflow&logoColor=white" />
+<img src="https://img.shields.io/badge/TensorFlow-Lite-3776AB?style=for-the-badge&logo=tensorflow&logoColor=white" /> 	
+<img src="https://img.shields.io/badge/TensorFlow-Js-3776AB?style=for-the-badge&logo=tensorflow&logoColor=white" /> 	
+<img src="https://img.shields.io/badge/Tensor-RT-3776AB?style=for-the-badge&logo=tensorflow&logoColor=white" /> 	
 <img src="https://img.shields.io/badge/PyTorch-3776AB?style=for-the-badge&logo=pytorch&logoColor=white" /> 
 <img src="https://img.shields.io/badge/Scikit-Learn-3776AB?style=for-the-badge&logo=scikit-learn&logoColor=white" /> 
 <img src="https://img.shields.io/badge/Opencv-3776AB?style=for-the-badge&logo=opencv&logoColor=white" /> 
@@ -46,30 +58,64 @@ For more information, guidance or help feel free to Connect. Let's reduce humun 
 <img src="https://img.shields.io/badge/Pandas-3776AB?style=for-the-badge&logo=pandas&logoColor=white" /> 
 <img src="https://img.shields.io/badge/Scipy-3776AB?style=for-the-badge&logo=scipy&logoColor=white" /> 
 <img src="https://img.shields.io/badge/Dlib-3776AB?style=for-the-badge&logo=dlib&logoColor=white" /> 
-<img src="https://img.shields.io/badge/Torch Vision-3776AB?style=for-the-badge&logo=torchvision&logoColor=white" /> 
-	
-</div>	
-<div>
-<h2>:writing_hand:NLP</h2>
+<img src="https://img.shields.io/badge/Torch Vision-3776AB?style=for-the-badge&logo=torchvision&logoColor=white" />
 <img src="https://img.shields.io/badge/Hugging-Face-3776AB?style=for-the-badge&logo=huggingface&logoColor=white" /> 
 <img src="https://img.shields.io/badge/gensim-3776AB?style=for-the-badge&logo=gensim&logoColor=white" /> 
 <img src="https://img.shields.io/badge/spacy-3776AB?style=for-the-badge&logo=spacy&logoColor=white" /> 
-<img src="https://img.shields.io/badge/transformer-3776AB?style=for-the-badge&logo=transformer&logoColor=white" /> 
+<img src="https://img.shields.io/badge/transformers-3776AB?style=for-the-badge&logo=transformers&logoColor=white" /> 
 <img src="https://img.shields.io/badge/NLTK-3776AB?style=for-the-badge&logo=nltk&logoColor=white" /> 
 <img src="https://img.shields.io/badge/ParlAI-3776AB?style=for-the-badge&logo=parlai&logoColor=white" /> 
+<img src="https://img.shields.io/badge/MatplotLib-3776AB?style=for-the-badge&logo=matplotlib&logoColor=white" /> 
+	<img src="https://img.shields.io/badge/DarkNet-3776AB?style=for-the-badge&logo=darknet&logoColor=white" /> 
+	<img src="https://img.shields.io/badge/Theano-3776AB?style=for-the-badge&logo=theano&logoColor=white" /> 
+	<img src="https://img.shields.io/badge/Deep-Stream-3776AB?style=for-the-badge&logo=deepstream&logoColor=white" />
+	<img src="https://img.shields.io/badge/Tesseract-3776AB?style=for-the-badge&logo=tesseract&logoColor=white" /> 
+	<img src="https://img.shields.io/badge/CNTK-3776AB?style=for-the-badge&logo=cntk&logoColor=white" /> 
+	<img src="https://img.shields.io/badge/Selenium-3776AB?style=for-the-badge&logo=selenium&logoColor=white" /> 
+	<img src="https://img.shields.io/badge/Allen-NLP-3776AB?style=for-the-badge&logo=allen-nlp&logoColor=white" /> 
+	<img src="https://img.shields.io/badge/Text-Blob-3776AB?style=for-the-badge&logo=text-blob&logoColor=white" /> 
+	<img src="https://img.shields.io/badge/Stanford-NLP-3776AB?style=for-the-badge&logo=stanford-nlp&logoColor=white" /> 
 </div>	
 <div>
-<h2>:file_cabinet: Python Frameworks</h2>
+<h2>Python Frameworks</h2>
 <img src="https://img.shields.io/badge/Django-3776AB?style=for-the-badge&logo=django&logoColor=white" /> 
 <img src="https://img.shields.io/badge/Flask-3776AB?style=for-the-badge&logo=flask&logoColor=white" /> 
 <img src="https://img.shields.io/badge/FastAPI-3776AB?style=for-the-badge&logo=fast-api&logoColor=white" /> 
+	<img src="https://img.shields.io/badge/Streamlit-3776AB?style=for-the-badge&logo=streamlit&logoColor=white" /> 
 </div>
 <div>
-<h2>:file_cabinet: Database and Caching Server</h2>
+<h2>:file_cabinet: Cloud and Storage</h2>
 <img src="https://img.shields.io/badge/MongoDB-3776AB?style=for-the-badge&logo=mongodb&logoColor=white" /> 
 <img src="https://img.shields.io/badge/Postgres-3776AB?style=for-the-badge&logo=postgres&logoColor=white" /> 
 <img src="https://img.shields.io/badge/SQLite-3776AB?style=for-the-badge&logo=sqlite&logoColor=white" /> 
 <img src="https://img.shields.io/badge/Redis-3776AB?style=for-the-badge&logo=redis&logoColor=white" />
+<img src="https://img.shields.io/badge/Github-3776AB?style=for-the-badge&logo=github&logoColor=white" /> 
+	<img src="https://img.shields.io/badge/GitLab-3776AB?style=for-the-badge&logo=gitlab&logoColor=white" /> 
+	<img src="https://img.shields.io/badge/Bitbucket-3776AB?style=for-the-badge&logo=bitcucket&logoColor=white" /> 
+	<img src="https://img.shields.io/badge/SVN-3776AB?style=for-the-badge&logo=svn&logoColor=white" /> 
+<img src="https://img.shields.io/badge/Cassandra-3776AB?style=for-the-badge&logo=cassandra&logoColor=white" /> 
+<img src="https://img.shields.io/badge/RabbitMQ-3776AB?style=for-the-badge&logo=rabbitmq&logoColor=white" /> 
+<img src="https://img.shields.io/badge/Kafka-3776AB?style=for-the-badge&logo=kafka&logoColor=white" /> 
+<img src="https://img.shields.io/badge/Nginx-3776AB?style=for-the-badge&logo=nginx&logoColor=white" /> 
+<img src="https://img.shields.io/badge/Apache-3776AB?style=for-the-badge&logo=apache&logoColor=white" /> 
+<img src="https://img.shields.io/badge/Docker-3776AB?style=for-the-badge&logo=docker&logoColor=white" /> 
+<img src="https://img.shields.io/badge/Kubernetes-3776AB?style=for-the-badge&logo=kubernetes&logoColor=white" /> 
+<img src="https://img.shields.io/badge/Infra-3776AB?style=for-the-badge&logo=infra&logoColor=white" /> 
+<img src="https://img.shields.io/badge/Github-Actions-3776AB?style=for-the-badge&logo=github-actions&logoColor=white" /> 
+<img src="https://img.shields.io/badge/Azure-CI-3776AB?style=for-the-badge&logo=azure-ci&logoColor=white" /> 
+<img src="https://img.shields.io/badge/Jenkins-3776AB?style=for-the-badge&logo=rabbitmq&logoColor=white" /> 
+<img src="https://img.shields.io/badge/Travis-3776AB?style=for-the-badge&logo=travis&logoColor=white" /> 
+</div>
+<div>
+	<h2>3D & AR </h2>
+	<img src="https://img.shields.io/badge/Unity-3776AB?style=for-the-badge&logo=unity&logoColor=white" /> 
+	<img src="https://img.shields.io/badge/3-js-3776AB?style=for-the-badge&logo=3js&logoColor=white" /> 
+</div>
+<div>
+	<h2> Cloud Providers </h2>
+	<img src="https://img.shields.io/badge/Azure-3776AB?style=for-the-badge&logo=azure&logoColor=white" /> 
+	<img src="https://img.shields.io/badge/AWS-3776AB?style=for-the-badge&logo=aws&logoColor=white" /> 
+	<img src="https://img.shields.io/badge/Google-Cloud-3776AB?style=for-the-badge&logo=gcp&logoColor=white" /> 
 </div>
 </br>
 <p align="left"> <a href="https://github.com/ryo-ma/github-profile-trophy"><img src="https://github-profile-trophy.vercel.app/?username=hrnbot" alt="hrnbot" /></a> </p>
