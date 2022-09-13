a14e6784bb8b945dc6160a1f15612bc1de7dcc52
Hiren Namera <30949020+hrnbot@users.noreply.github.com>
2022-09-13 13:26:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -146,6 +146,7 @@ For more information, guidance or help feel free to Connect. Let's reduce humun 
 <h11>Where to find me</h1>
 <p><a href="https://github.com/hrnbot" target="_blank"><img alt="Github" src="https://img.shields.io/badge/GitHub-%2312100E.svg?&style=for-the-badge&logo=Github&logoColor=white" /></a> <a href="https://www.linkedin.com/in/hiren-namera-83520ab4" target="_blank"><img alt="LinkedIn" src="https://img.shields.io/badge/linkedin-%230077B5.svg?&style=for-the-badge&logo=linkedin&logoColor=white" /></a> <a href="https://www.instagram.com/the_artificial_intel_developer/" target="_blank"><img alt="Instagram" src="https://img.shields.io/badge/instagram-%23E4405F.svg?&style=for-the-badge&logo=instagram&logoColor=white" /></a>
 </p>
+<a href="https://www.buymeacoffee.com/hrnbot" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/default-orange.png" alt="Buy Me A Coffee" height="41" width="174"></a>
 <p><a href="https://github.com/hrnbot" target="_blank"><img src="https://github.com/hrnbot/hrnbot/workflows/Progress%20Bar%20CI/badge.svg"/></a></p>
 <p>
 <p>⏰ Updated on ${new Date().toUTCString()}</p>
