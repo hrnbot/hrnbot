4f482564bfdae8c1b92090d361518cb6899429cb
Hiren Namera <30949020+hrnbot@users.noreply.github.com>
2023-09-14 14:20:50 +0530

    Update index.js
    Removed introduction

--- index.js
+++ index.js
@@ -16,24 +16,6 @@ function generateProgressBar() {
 
 const readme = `\
 <h1>Hi there <img src="https://media.giphy.com/media/hvRJCLFzcasrR4ia7z/giphy.gif" height="25px" width="25px">,  Hiren Namera here</h1>
-
-<h1><details><summary>
-Who am I.
-<p><img src="https://komarev.com/ghpvc/?username=hrnbot&label=Profile%20views&color=0e75b6&style=flat" alt="hrnbot" /> </p>
-	</summary><h5>
-My Skills are improving day by day. Mathematics and  Physics are the skills in which I am proud d of most. Programming is also one of the best skills to solve problems in the IT world.  I never knew when I become proficient in writing maybe it is the Result of my Reading habit which I developed after my college life. I am an Introvert who loves to meet new peoples and learn new skills from them. I am waiting for a new education policy to be adapted So, I can complete my Ph.D. in Applied Mathematics and Artificial Intelligence.
-
-I am working in the field of AI/ML and AR. I am a Detail-oriented engineer, with get-it-done, on-time, and best quality products. I had proven my skills in AI by modeling some state of art architectures and algorithms and proved skills of AR by writing multi-purpose AR modules that work seamlessly on Js, Python, C#, CPP, PHP (More to Come).
-
-As a senior developer in AI/ML/DL/computer vision and NLP learned how to train and test model architectures in Pytorch, 
-	, Tensorflow, Theano, Deepstream, Tensorflow lite, TensorRT, Transformer etc... How to perform transfer learning for quick training. How to Combine multiple models and how to change architectures of underlying layers, build model architecture from scratch, etc... 
-
-As a team leader of the AI and AR team learned to manage a team and take most out of team members. Train them to accept and complete future challenges and helping them to improve those skills in which they are lacking. Also learned how to use agile methodology to complete any project while keeping the team motivated and client satisfied. I was responsible for understanding client requirements and business prototype and help them to solve their problem in a modern way including enhancement of user experience, financial growth, increase in production capacity or productivity of employees, reach out to more customers, etc...
-
-I was also responsible to deploy AI, AR Technology to Mobile, Cloud, and web. So, I am capable of performing basic DevOps duties of CI and CD pipeline building, Rest-API handling, Deployment of the model, and balancing load using tech stack of Git/SVN, Jenkins, Ansible, Docker, Kubernetes/Docker Swarm in AWS, Azure and Google Cloud.
-
-For more information, guidance or help feel free to Connect. Let's reduce humun effort with Power of AI and AR and Build a Sci-fi World. Peace...
-</h5></details>
 </h1>
 <div>
 <h1><b>Skills</b></h1>
